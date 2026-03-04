212e9b890d237fff3a835e53297fe954bc40d11e
Mussa Faragi <mussa.faragi@gmx.de>
2026-03-04 20:43:31 +0100

    init class

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
+
 
 function init(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     
-    character.src = "img/...";
+    
 
 }
