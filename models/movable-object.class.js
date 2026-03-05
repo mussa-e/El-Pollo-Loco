c5a5142caa3d13633374fe47e2d2e37186a114b4
Mussa Faragi <mussa.faragi@gmx.de>
2026-03-05 21:05:20 +0100

    character draw

---
+++ models/movable-object.class.js
@@ -0,0 +1,22 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 150;
+    width = 100;
+
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    moveRight(){
+        console.log("moving right");
+    }
+
+    moveLeft(){
+        
+    }
+}
