9d3fdbc351de39b203ee6aaddcffe09758db62f5
Mussa Faragi <mussa.faragi@gmx.de>
2026-03-08 10:38:30 +0100

    background added

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,12 @@
 class Character extends MovableObject {
-
-
+    height = 200;
+    width = 120;
+    y = 225;
+    
     constructor(){
         super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+
+        
     }
 
     jump(){
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,9 +1,13 @@
 class Chicken extends MovableObject{
+    height = 90;
+    width = 70;
+    y = 335;
 
     constructor(){
         super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     
         this.x = 200 + Math.random() * 500;
+        
     }
     
 } 
